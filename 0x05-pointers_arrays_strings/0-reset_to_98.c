bed40a5530f40e4268a86bc965157b2402de164e
Emiechris <ugwumartins72@gmail.com>
2022-07-14 04:32:31 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+* reset_to_98 - Sets the pointer value to 98
+*@n: pointer declaration
+*Returns -Void
+*/
+void reset_to_98(int *n)
+	{
+	*n = 98;
+	}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - print the length of a given string
+* @s: string variable
+*
+* Return: length of the string
+*/
+
+	int  _strlen(char *s)
+	{
+		return (strlen(s));
+	}
